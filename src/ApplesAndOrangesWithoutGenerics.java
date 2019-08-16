8cd1a6d2c4e8c2f90fdbb0b79fadb721873f5535
Plankton <mabinogi1996@iCloud.com>
2019-08-16 15:57:18 +0800

    testbranch

--- src/ApplesAndOrangesWithoutGenerics.java
+++ src/ApplesAndOrangesWithoutGenerics.java
@@ -1,9 +1,30 @@
-import java.util.ArrayList;
+import org.junit.Test;
+
+
+import java.util.*;
+
 
 public class ApplesAndOrangesWithoutGenerics {
+
     public static void main(String[] agrs){
-        ArrayList apples = new ArrayList();
+        LinkedList<Apple> apples = new LinkedList<Apple>();
+
+        for (int i = 0; i < 3; i++) {
+            apples.add(new Apple());
+        }
+//        apples.add(new Orange());
+        Deque<String> stack = new ArrayDeque<>();
+        String str = "My dog has fleas";
+
+        for(String s : "My dog has fleas".split(" "))
+            stack.push(s);
+        while(!stack.isEmpty())
+            System.out.print(stack.pop() + " ");
+
     }
+
+
+
 }
 
 class Apple {
@@ -11,7 +32,7 @@ class Apple {
     private final long id = count++;
     public long id(){return this.id;}
 }
-class orange{
+class Orange {
 
 }
 
